bb2bd9d43ecde9f29cb0ff62435adc5d1bb5a8bb
commissofederico88@gmail.com <commissofederico88@gmail.com>
2025-01-24 14:05:10 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import './scss/base.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
--- src/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-
-const App = () => <h1>Welcome to Free Joint Motion</h1>;
-
-createRoot(document.getElementById("root")).render(<App />);
