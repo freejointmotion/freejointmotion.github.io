1972a54a4aacffcff87a97f072aba8669ab478ae
commissofederico88@gmail.com <commissofederico88@gmail.com>
2025-01-24 20:41:32 -0500

    prerendering

---
+++ prerender.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  pages: [
+    {
+      url: "/",
+      output: "index.html"
+    },
+    {
+      url: "/about",
+      output: "about/index.html"
+    },
+    {
+      url: "/sectors",
+      output: "sectors/index.html"
+    },
+    {
+      url: "/knowledge",
+      output: "knowledge/index.html"
+    },
+    {
+      url: "/contact",
+      output: "contact/index.html"
+    }
+  ]
+};
